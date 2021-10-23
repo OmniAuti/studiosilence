3f42b9e06e903e4d153bc4895032fc030159572c
Austin Olijar <73905380+OmniAuti@users.noreply.github.com>
2021-10-22 19:36:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 const shapeOne = document.querySelector('.starting-shape-one')
+const shapeOnea = document.querySelector('.starting-shape-one-2')
+const shapeOneb = document.querySelector('.starting-shape-one-3')
+const shapeOnec = document.querySelector('.starting-shape-one-4')
+const shapeOned = document.querySelector('.starting-shape-one-5')
 const shapeTwo = document.querySelector('.starting-shape-two')
 const shapeThree = document.querySelector('.starting-shape-three')
 
 
 const aboutSection = document.querySelector('.about-section')
-
+const hero = document.querySelector('.hero')
 
 const wrapper = document.querySelector('main')
 
 wrapper.addEventListener('scroll', () => {
 
 
-
-    console.log(aboutSection.getBoundingClientRect())
-    console.log(aboutSection.getBoundingClientRect())
+    const paraShape = hero.getBoundingClientRect().x * 1;
+    console.log(paraShape * -10)
+    shapeOne.style.transform = `translateX(${paraShape * -1}px)`
+    shapeOnea.style.transform = `translateX(${paraShape * -1}px)`
+    shapeOneb.style.transform = `translateX(${paraShape * -1}px)`
+    shapeOnec.style.transform = `translateX(${paraShape * -1}px)`
+    shapeOned.style.transform = `translateX(${paraShape * -1}px)`
 
    if (aboutSection.getBoundingClientRect().left < 325)  {
 
-        shapeOne.classList.add('moveOneAbout') 
+
         shapeTwo.classList.add('moveTwoAbout') 
         shapeThree.classList.add('moveThreeAbout') 
 
    } 
    else if (aboutSection.getBoundingClientRect().left > 325) {
-       shapeOne.classList.remove('moveOneAbout')
        shapeTwo.classList.remove('moveTwoAbout')
        shapeThree.classList.remove('moveThreeAbout')
    }
@@ -36,3 +43,4 @@ scrollContainer.addEventListener('wheel', (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
 });
+
